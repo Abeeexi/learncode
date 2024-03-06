cfcd17e5824aaeba80c48a07426595950a62520e
Abeeexi <nguyentrananhbang@gmail.com>
2024-03-07 00:22:28 +0700

    git

--- index.ts
+++ index.ts
@@ -1,13 +1,23 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
-const app = express();
+function main() {
+  const prisma = new PrismaClient();
+  const app = express();
 
-app.get("/", (req, res)=> {
-  
-  res.send("hello");
-});
+  app.get("/", async (req, res) => {
+    // prisma.user.findUnique({
+    //   where: {
+    //     id: 1,
+    //   },
+    // });
+    const users = prisma.user.findMany();
+    res.send(users);
+  });
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Listen at localhost:${port}`);
-});
+  const port = 3000;
+  app.listen(port, () => {
+    console.log(`Listen at localhost:${port}`);
+  });
+}
+main();
